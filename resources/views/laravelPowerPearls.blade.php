bee8a7fb18ab773354d1c522135bf3ec096278ee
Chris-Th <muhmuhmiau@proton.me>
2023-03-20 14:14:28 +0100

    first commit

---
+++ resources/views/laravelPowerPearls.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Power Pearls edition</title>
+</head>
+<body>
+    <h1>Laravel Power Pearls edition</h1>
+    <h3>The next generation of Laravel's powerful detergents and fluffy laundry softeners has finally arrived!</h3>
+    <p>say goodbye to fiber loss with Laravels next generation Fiber-Retainer - your lingerie will last a lifetime</p>
+</body>
+</html>
