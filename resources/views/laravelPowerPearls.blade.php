520d8922c80acd0484e15479b57af1e5cc3a8e8f
Chris-Th <muhmuhmiau@proton.me>
2023-03-20 15:51:53 +0100

    123

--- resources/views/laravelPowerPearls.blade.php
+++ resources/views/laravelPowerPearls.blade.php
@@ -8,7 +8,16 @@
 </head>
 <body>
     <h1>Laravel Power Pearls edition</h1>
+   
+    <!-- extend layout master.php -->
+    @extends('layouts.master')
+
+    <!-- sets value for section title to "Laravel Power Pearls" (section title is used as yield in messages.blade.php) -->
+    @section('title', 'Laravel Power Pearls' )
+
     <h3>The next generation of Laravel's powerful detergents and fluffy laundry softeners has finally arrived!</h3>
     <p>say goodbye to fiber loss with Laravels next generation Fiber-Retainer - your lingerie will last a lifetime</p>
+
+    @endsectiom
 </body>
 </html>
